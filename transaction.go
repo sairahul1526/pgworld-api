c1c093977f46fb3dbca5c14da5e84ed614c4a055
sairahul1526 <dravid.rahul1526@gmail.com>
2019-09-04 00:06:31 +0530

    make user joined when rent add: forgot to add columns like documents, type, payment mode

--- transaction.go
+++ transaction.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // transaction
@@ -22,17 +23,24 @@ func Rent(w http.ResponseWriter, r *http.Request) {
 		body[key] = value[0]
 	}
 
-	status, ok := updateSQL(userTable, url.Values{"hostel_id": {body["hostel_id"]}, "id": {body["user_id"]}}, map[string]string{"last_paid_date_time": body["paid_date_time"], "expiry_date_time": body["expiry_date_time"]})
+	update := map[string]string{"last_paid_date_time": body["paid_date_time"], "expiry_date_time": body["expiry_date_time"]}
+	if strings.EqualFold(body["joining"], "1") {
+		update["joining"] = "0"
+	}
+	status, ok := updateSQL(userTable, url.Values{"hostel_id": {body["hostel_id"]}, "id": {body["user_id"]}}, update)
 	w.Header().Set("Status", status)
 	if ok {
 		if len(body["bill_id"]) == 0 {
 			// log
 			logAction(body["admin_name"], "accepted rent ₹"+body["amount"]+" from "+body["name"], "7", body["hostel_id"])
-			insertSQL(billTable, map[string]string{"hostel_id": body["hostel_id"], "user_id": body["user_id"], "title": body["title"], "description": body["description"], "amount": body["amount"], "paid_date_time": body["paid_date_time"], "status": "1", "paid": "0"})
+			insertSQL(billTable, map[string]string{"hostel_id": body["hostel_id"], "user_id": body["user_id"], "title": body["title"], "description": body["description"], "amount": body["amount"], "paid_date_time": body["paid_date_time"], "status": "1", "paid": "0", "document": body["document"], "type": body["type"], "payment": body["payment"], "transaction_id": body["transaction_id"]})
 		} else {
 			// log
 			logAction(body["admin_name"], "updated rent ₹"+body["amount"]+" from "+body["name"], "7", body["hostel_id"])
-			updateSQL(billTable, url.Values{"id": {body["bill_id"]}, "hostel_id": {body["hostel_id"]}, "user_id": {body["user_id"]}}, map[string]string{"amount": body["amount"], "paid_date_time": body["paid_date_time"]})
+			updateSQL(billTable, url.Values{"id": {body["bill_id"]}, "hostel_id": {body["hostel_id"]}, "user_id": {body["user_id"]}}, map[string]string{"amount": body["amount"], "paid_date_time": body["paid_date_time"], "document": body["document"], "type": body["type"], "payment": body["payment"], "transaction_id": body["transaction_id"]})
+		}
+		if strings.EqualFold(body["joining"], "1") {
+			db.Exec("update " + roomTable + " set room_joining = room_joining - 1 where id = '" + body["room_id"] + "' and room_joining > 0")
 		}
 		response["meta"] = setMeta(status, "User updated", dialogType)
 	} else {
@@ -67,11 +75,11 @@ func Salary(w http.ResponseWriter, r *http.Request) {
 		if len(body["bill_id"]) == 0 {
 			// log
 			logAction(body["admin_name"], "gave salary ₹"+body["amount"]+" to "+body["name"], "8", body["hostel_id"])
-			insertSQL(billTable, map[string]string{"hostel_id": body["hostel_id"], "employee_id": body["employee_id"], "title": body["title"], "description": body["description"], "amount": body["amount"], "paid_date_time": body["paid_date_time"], "status": "1", "paid": "1"})
+			insertSQL(billTable, map[string]string{"hostel_id": body["hostel_id"], "employee_id": body["employee_id"], "title": body["title"], "description": body["description"], "amount": body["amount"], "paid_date_time": body["paid_date_time"], "status": "1", "paid": "1", "document": body["document"], "type": body["type"], "payment": body["payment"], "transaction_id": body["transaction_id"]})
 		} else {
 			// log
 			logAction(body["admin_name"], "updated rent ₹"+body["amount"]+" to "+body["name"], "8", body["hostel_id"])
-			updateSQL(billTable, url.Values{"id": {body["bill_id"]}, "hostel_id": {body["hostel_id"]}, "employee_id": {body["employee_id"]}}, map[string]string{"amount": body["amount"], "paid_date_time": body["paid_date_time"]})
+			updateSQL(billTable, url.Values{"id": {body["bill_id"]}, "hostel_id": {body["hostel_id"]}, "employee_id": {body["employee_id"]}}, map[string]string{"amount": body["amount"], "paid_date_time": body["paid_date_time"], "document": body["document"], "type": body["type"], "payment": body["payment"], "transaction_id": body["transaction_id"]})
 		}
 		response["meta"] = setMeta(status, "User updated", dialogType)
 	} else {
